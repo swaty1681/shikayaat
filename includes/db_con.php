e3a5199859e1a45aec51b9e81089e65f8fd7668f
Swaty Mohanty <swatymohanty1681@gmail.com>
2023-09-03 02:38:02 +0530

    Added shikayaat to github

---
+++ includes/db_con.php
@@ -0,0 +1,16 @@
+<?php
+
+    // require_once ___INC___.'config.php';
+
+    // $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD) or die('Error connecting to MySQL server.');
+
+    // mysqli_select_db($conn, DB) or die('Error selecting database.');
+    // mysqli_select_db($conn, DB) or die('Error selecting database.');
+    
+    
+    
+    // get mysqli connection
+    include_once ___INC___."functions.php";
+    $conn = get_conn();
+
+?>
---
+++ includes/pages/support.php
@@ -0,0 +1,23 @@
+<div class="content-wrapper">
+
+    <div class="card full-width">
+
+        <h1 class="card-heading">Support</h1>
+
+        <div class="card-content">
+
+            <div class="texts-holder">
+                <p class="font-size-25">
+                    <b>For Technical Support:</b> <br>
+                    &emsp; Swaty Mohanty <br>
+                    &emsp; B.Tech 3rd Year, CSIT <br>
+                    &emsp; Contact No: <a href="tel:7008044462">+91-7008044462</a> <br>
+                    &emsp; Email: <a href="mailto:swatymohanty1681@gmail.com">swatymohanty1681@gmail.com</a>
+                </p>
+                
+            </div>
+        </div>
+
+    </div>
+
+</div>
